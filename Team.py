8dc6a90076bc463a0754099ec851f44d40f89090
Ali Muhammad Afzal <52150966+aleeraii@users.noreply.github.com>
2020-04-23 17:42:48 +0500

    Add files via upload

---
+++ Team.py
@@ -0,0 +1,9 @@
+class Team:
+
+    fee = 1000
+    # Initializer / Instance Attributes
+    def __init__(self, name, city, is_paid, num_players):
+        self.name = name
+        self.city = city
+        self.is_paid = is_paid
+        self.num_players = num_players
